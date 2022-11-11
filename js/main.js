b85920b17a879a85634cbe8a3bfcc517b91ec90c
DainiusMI <mental.def@gmail.com>
2022-11-11 14:08:15 +0200

    delete hover color fix

--- js/main.js
+++ js/main.js
@@ -7,27 +7,47 @@ function renderComments() {
     let generatedString = "";
    
     dataFile.comments.map(comment => {
+
         function populateComment(arg) {
-            return `
+            let string = "";
+            string +=`
             <div class="comment-container">
                 <div class="vote-container">
                     <button class="upvote" value="${arg.id}"><img src="./images/icon-plus.svg" alt="+"></button>
                     <p class="vote-score">${arg.score}</p>
-                    <button class="downvote" value="${arg.id}><img src="./images/icon-minus.svg" alt="-"></button>
+                    <button class="downvote" value="${arg.id}"><img src="./images/icon-minus.svg" alt="-"></button>
                 </div>
                 <div class="user-info">
                     <div class="avatar">
                         <img src="${arg.user.image.png}" alt="${arg.user.username}-img">
                     </div>
                     <div class="user-name">${arg.user.username}</div>
+            `
+            if (dataFile.currentUser.username === arg.user.username) {
+                string += `<div class="you">you</div>`
+            }
+            string += `
                     <div class="created-at">${arg.createdAt}</div>
                 </div>
                 <p class="comment">${arg.content}</p>
                 <div class="button-container">
-                    <button class="action-button"><img src="./images/icon-reply.svg" alt="reply-img"><p class="reply">Reply</p></button>
+            `
+            if (dataFile.currentUser.username === arg.user.username) {
+                string += `
+                    <button class="action-button red"><img class="" src="./images/icon-delete.svg" alt="reply-img"><p class="delete">Delete</p></button>
+                    <button class="action-button blue"><img src="./images/icon-edit.svg" alt="reply-img"><p class="edit">Edit</p></button>
+                `
+            }
+            else {
+                string += `
+                    <button class="action-button blue"><img src="./images/icon-reply.svg" alt="reply-img"><p class="reply">Reply</p></button>
+                `
+            }
+            string += `
                 </div>
             </div>
-        `
+            `
+            return string
         }
         generatedString += populateComment(comment);
         if (comment.replies.length) {
@@ -44,7 +64,7 @@ function renderComments() {
 function renderPostForm() {
     appContainer.innerHTML += `
         <div class="form-container">
-            <div class="avatar"><img src="${dataFile.currentUser.image.png}" alt="julius"></div>
+            <div class="avatar"><img src="${dataFile.currentUser.image.png}" alt="${dataFile.currentUser.username}"></div>
             <textarea class="textarea" name="post" id="post" cols="30" rows="7"></textarea>
             <button class="blue-button">SEND</button>
         </div>
